5d924903282f2f281a4f22dad49676727ce5c0fb
CoreyJNash <CoreyJNash@gmail.com>
2018-07-09 19:27:12 -0400

    removed console logs

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -5,25 +5,25 @@ const onCreateSuccess = function (data) {
   $('#message').text('Game successfully created')
   $('#message').css('background-color', '#f7bd2f')
   store.game = data.game
-  console.log('onCreateSuccess ran. Data is :', data)
+  // console.log('onCreateSuccess ran. Data is :', data)
 }
 
 const onCreateFailure = function (error) {
   $('#message').text('Error on creating Game')
   $('#message').css('background-color', 'red')
-  console.error('onCreateFailure ran. Error is :', error)
+  // console.error('onCreateFailure ran. Error is :', error)
 }
 
 const onIndexSuccess = function (data) {
   $('#message').text('All Games successfully received')
   $('#message').css('background-color', '#f7bd2fn')
-  console.log('onIndexSuccess ran. Data is :', data.examples)
+  // console.log('onIndexSuccess ran. Data is :', data.examples)
 }
 
 const onIndexFailure = function (error) {
   $('#message').text('Error on getting Games')
   $('#message').css('background-color', 'red')
-  console.error('onIndexFailure ran. Error is :', error)
+  // console.error('onIndexFailure ran. Error is :', error)
 }
 
 const showGamesSucess = function (data) {
@@ -31,7 +31,7 @@ const showGamesSucess = function (data) {
     $('#view-games').append('<p><b>ID:</b>' + data.games[i].id + '    <b>Game Squares</b>' + data.games[i].cells + '</p>')
     $('#view-games').css('color', '#f6ff18') 
     $('#view-games').removeClass('hidden')
-    console.log(data.games[i])
+    // console.log(data.games[i])
   }
   // $('#view-games').text('${data.games} games have been completed!')
   // console.log(data.games)
@@ -40,7 +40,7 @@ const showGamesSucess = function (data) {
 const showGamesFailure = function (error) {
   $('#message').text('Error on getting games')
   $('#message').css('background-color', 'red')
-  console.error('onShowFailure ran. Error is :', error)
+  // console.error('onShowFailure ran. Error is :', error)
 }
 
 const userMovesSucess = function (data) {
@@ -50,7 +50,7 @@ const userMovesSucess = function (data) {
 const onUpdateFailure = function (error) {
   $('#message').text('Error on updating Game')
   $('#message').css('background-color', 'red')
-  console.error('onUpdateFailure ran. Error is :', error)
+  // console.error('onUpdateFailure ran. Error is :', error)
 }
 
 module.exports = {
