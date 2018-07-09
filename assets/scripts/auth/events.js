b72068897cc395a61a0539f29b883fee11adb1a6
CoreyJNash <CoreyJNash@gmail.com>
2018-07-09 19:21:19 -0400

    removed console logs

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -7,7 +7,7 @@ const ui = require('./ui')
 
 const onSignUp = function (event) {
   event.preventDefault()
-  console.log('sign up ran!')
+  // console.log('sign up ran!')
 
   const data = getFormFields(this)
   api.signUp(data)
@@ -17,7 +17,7 @@ const onSignUp = function (event) {
 
 const onSignIn = function (event) {
   event.preventDefault()
-  console.log('sign in ran!')
+  // console.log('sign in ran!')
 
   const data = getFormFields(this)
   api.signIn(data)
@@ -27,7 +27,7 @@ const onSignIn = function (event) {
 
 const onSignOut = function (event) {
   event.preventDefault()
-  console.log('sign out ran')
+  // console.log('sign out ran')
 
   api.signOut()
     .then(ui.signOutSuccess)
@@ -36,7 +36,7 @@ const onSignOut = function (event) {
 
 const onChangePassword = function (event) {
   event.preventDefault()
-  console.log('change password ran!')
+  // console.log('change password ran!')
 
   const data = getFormFields(this)
   api.changePassword(data)
